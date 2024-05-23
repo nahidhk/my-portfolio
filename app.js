d4f5861ce2bc46597dcc1e5a6e0744fa8059ea13
Nahid HK <nahidappltd@gmail.com>
2024-05-23 08:07:20 +0600

    new update

---
+++ app.js
@@ -0,0 +1,39 @@
+const typedText = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+
+const textArray = ["a Student .","Web Developer .", "Electric Circuit Development.", "Canva Design .", "Python Machine Learning .","Technical .","Digital Technology Management."];
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+const erase = () => {
+  if (charIndex > 0) {
+    cursor.classList.remove('blink');
+    typedText.textContent = textArray[textArrayIndex].slice(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 80);
+  } else {
+    cursor.classList.add('blink');
+    textArrayIndex++;
+    if (textArrayIndex > textArray.length - 1) {
+      textArrayIndex = 0;
+    }
+    setTimeout(type, 700);
+  }
+}
+
+const type = () => {
+  if (charIndex <= textArray[textArrayIndex].length - 1) {
+    cursor.classList.remove('blink');
+    typedText.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 120);
+  } else {
+    cursor.classList.add('blink');
+    setTimeout(erase, 700);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  type();
+})
